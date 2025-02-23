c3b13d54c0f5b47e3b10194cb0b9e1f41dcbf49b
721150 <144417455+721150@users.noreply.github.com>
2025-02-23 13:44:29 +0100

    v5
    Toevoegen contactmogelijkheid

---
+++ src/components/InputField.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  label: String,
+  modelValue: String,
+  type: {
+    type: String,
+    default: 'text'
+  },
+  placeholder: String
+});
+
+function updateValue(event) {
+  emit('update:modelValue', event.target.value);
+}
+</script>
+
+<template>
+  <div class="mb-3">
+    <label :for="label" class="form-label">{{ label }}</label>
+    <input :type="type" :id="label" class="form-control" :value="modelValue" :placeholder="placeholder" @input="updateValue">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,9 +1,67 @@
 <script setup>
+import { ref } from 'vue';
+import SelectField from '../components/SelectField.vue';
+import InputField from '../components/InputField.vue';
 
+const name = ref('');
+const email = ref('');
+const question = ref('');
+const institution = ref('');
+const course = ref('');
+const topic = ref('');
+const rightType = ref('');
+const files = ref([]);
+
+const institutions = ['Instelling A', 'Instelling B', 'Instelling C'];
+const courses = ['Opleiding X', 'Opleiding Y', 'Opleiding Z'];
+const topics = ['Onderwerp 1', 'Onderwerp 2', 'Onderwerp 3'];
+const rightTypes = ['Recht A', 'Recht B', 'Recht C'];
+
+function handleFileUpload(event) {
+  const newFiles = Array.from(event.target.files);
+  files.value = files.value.concat(newFiles);
+}
+
+function submitForm() {
+  console.log('Form submitted:', {
+    name: name.value,
+    email: email.value,
+    question: question.value,
+    institution: institution.value,
+    course: course.value,
+    topic: topic.value,
+    rightType: rightType.value,
+    files: files.value
+  });
+}
 </script>
 
 <template>
-
+  <div class="container mt-5">
+    <h2>Leg een casus voor over je rechten</h2>
+    <InputField label="Voornaam" v-model="firstname" placeholder="Voornaam" />
+    <InputField label="Achternaam" v-model="lastname" placeholder="Achternaam" />
+    <InputField label="E-mailadres" v-model="email" type="email" placeholder="E-mailadres" />
+    <SelectField label="Instelling" v-model="institution" :options="institutions" />
+    <SelectField label="Opleiding" v-model="course" :options="courses" />
+    <SelectField label="Onderwerp" v-model="topic" :options="topics" />
+    <SelectField label="Soort recht" v-model="rightType" :options="rightTypes" />
+    <div class="mb-3">
+      <label for="question" class="form-label">Casus</label>
+      <textarea v-model="question" class="form-control" id="question" rows="3" placeholder="Beschrijf je casus"></textarea>
+    </div>
+    <div class="mb-3">
+      <label for="file" class="form-label">Documenten</label>
+      <input type="file" class="form-control" id="files" multiple @change="handleFileUpload">
+    </div>
+    <div class="mb-3">
+      <h5>Geselecteerde bestanden:</h5>
+      <ul>
+        <li v-for="file in files" :key="file.name">{{ file.name }}</li>
+      </ul>
+    </div>
+    <button class="btn btn-success" @click="submitForm">Verstuur</button>
+  </div>
 </template>
 
 <style scoped>
