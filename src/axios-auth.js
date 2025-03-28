26a4e10466ae108e3b60f45bbf2e34e241096e5b
721150 <144417455+721150@users.noreply.github.com>
2025-03-28 09:27:31 +0100

    v34
    Oplossen inlogprobleem

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,12 +1,20 @@
 import axios from "axios";
 
-const token = localStorage.getItem("jwt");
-
 const instance = axios.create({
-  baseURL: 'http://localhost',
-  headers: {
-    Authorization: token ? `Bearer ${token}` : ''
+  baseURL: 'http://localhost'
+});
+
+instance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem("jwt");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
   }
-})
+);
 
 export default instance;
